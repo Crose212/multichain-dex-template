55e9595c78e20e991af1728ec1719a4dc0488d9d
Crose212 <crose1122333@gmail.com>
2023-09-08 05:41:58 +0000

    fix

--- src/interfaces/IPool.sol
+++ src/interfaces/IPool.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
+
+interface IPool {
+    
+
+    function tokenBalance(address token) external view returns (uint256);
+
+    function saveStuckToken(address stuckToken) external;
+
+    function migrateToNewPool(
+        address pool, 
+        address token
+    ) external;
+
+}
